629a8a11149dc90a0917253c36577386dc8816fb
Travis CI User <travis@example.org>
2018-05-31 20:57:50 +0000

    Update docs after building Travis build 224 of pcdshub/hutch-python
    The docs were built from the branch 'master' against the commit
    5e3aa362af93bde853eabe6d3417b1058dec4946.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pcdshub/hutch-python/jobs/386367227.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.5.0+9.g0c0c897',
+    VERSION: '0.5.0+12.g5e3aa36',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
