dd5ab981de67cb903da58f091b14eb27ca9b1337
Zachary Lentz <zlentz@slac.stanford.edu>
2018-07-12 11:35:34 -0700

    ENH: Refactor cam_load to use threading so that cameras that can't connect fail in parallel

--- hutch_python/cam_load.py
+++ hutch_python/cam_load.py
@@ -2,10 +2,16 @@
 Load Detector objects based on a camviewer config file
 """
 import logging
+from functools import partial
+from multiprocessing.pool import ThreadPool
+from multiprocessing import cpu_count
+
+from .constants import SUCCESS_LEVEL
 
 from pcdsdevices.areadetector.detectors import PCDSDetector
 
 logger = logging.getLogger(__name__)
+logger.success = partial(logger.log, SUCCESS_LEVEL)
 
 
 def read_camviewer_cfg(filename):
@@ -19,8 +25,26 @@ def read_camviewer_cfg(filename):
 
     Returns
     -------
-    dict: ``{str: PCDSDetector}``
+    objs: ``{str: PCDSDetector}``
         Each detector object, indexed by name.
+    """
+    info = interpret_cfg(filename)
+    return load_cams(info)
+
+
+def interpret_cfg(filename):
+    """
+    Read camviewer.cfg file and interpret lines
+
+    Parameters
+    ----------
+    filename: ``str``
+        Full path to the camviewer.cfg file.
+
+    Returns
+    -------
+    info: ``list of str``
+        Valid inputs for build_cam
     """
     with open(filename, 'r') as f:
         lines = f.readlines()
@@ -39,43 +63,79 @@ def interpret_lines(lines):
 
     Returns
     -------
-    dict: ``{str: PCDSDetector}``
-        Each detector object, indexed by name.
+    info: ``list of list str``
+        Valid inputs for build_cam
     """
-    objs = {}
+    info = []
 
     for line in lines:
         line = line.strip()
         if not line or line[0] == '#':
             continue
 
+        logger.debug(line)
         parts = line.split(',')
         parts = [p.strip() for p in parts]
 
         if parts[0].startswith('include'):
             try:
-                objs.update(read_camviewer_cfg(parts[0].split(' ')[1]))
+                inc = interpret_cfg(parts[0].split(' ')[1])
+                info.extend(inc)
             except IndexError:
                 err = 'Malformed include line "%s" in camviewer cfg, skipping.'
                 logger.error(err, line)
-        else:
-            try:
-                new_obj = build_cam(*parts)
-                objs[new_obj.name] = new_obj
-                logger.debug('Loaded %s', new_obj.name)
-            except UnsupportedConfig as exc:
-                logger.debug('Skip non area detector cam %s',
-                             exc.name, exc_info=True)
-            except MalformedConfig as exc:
-                err = 'Skip malformed config for cam %s'
-                logger.error(err, exc.name)
-                logger.debug(err, exc.name, exc_info=True)
-            except TypeError:
-                err = 'Malformed cam line "%s" in camviewer cfg, skipping.'
-                logger.error(err, line)
                 logger.debug(err, line, exc_info=True)
+        else:
+            info.append(parts)
 
+    return info
+
+
+def load_cams(info):
+    """
+    Create detector object from string lines of a camviewer.cfg file.
+
+    Parameters
+    ----------
+    info: ``list of list of str``
+        Valid inputs for build_cam
+
+    Returns
+    -------
+    objs: ``{str: PCDSDetector}``
+        Each detector object, indexed by name.
+    """
+    objs = {}
+    logger.debug(info)
+    pool = ThreadPool(cpu_count()-1)
+    obj_list = pool.map(build_and_log, info)
+    for obj in obj_list:
+        if obj is not None:
+            objs[obj.name] = obj
     return objs
+
+
+def build_and_log(info_part):
+    try:
+        obj = build_cam(*info_part)
+        logger.success('Loaded %s', obj.name)
+        return obj
+    except UnsupportedConfig as exc:
+        logger.debug('Skip non area detector cam %s',
+                     exc.name, exc_info=True)
+    except MalformedConfig as exc:
+        err = 'Skip malformed config %s'
+        logger.error(err, info_part)
+        logger.debug(err, info_part, exc_info=True)
+    except TypeError:
+        err = 'Malformed config %s in camviewer cfg, skipping.'
+        logger.error(err, info_part)
+        logger.debug(err, info_part, exc_info=True)
+    except Exception:
+        err = ('Error loading config %s in camviewer cfg. IOC '
+               'probably down, skipping.')
+        logger.error(err, info_part)
+        logger.debug(err, info_part, exc_info=True)
 
 
 def build_cam(cam_type, pv_info, evr, name, *args):
