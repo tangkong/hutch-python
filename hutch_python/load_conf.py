faaca806700b8792b102e81d7c0749129a6c8eb0
Adam <aegger@slac.stanford.edu>
2020-08-31 11:08:57 -0700

    quiet flake8 F401 error

--- hutch_python/load_conf.py
+++ hutch_python/load_conf.py
@@ -9,8 +9,7 @@ from pathlib import Path
 from socket import gethostname
 from types import SimpleNamespace
 
-import matplotlib
-matplotlib.use('Qt5Agg')  # noqa isort:skip
+from . import mpl_config  # noqa: F401 # isort: ignore
 from bluesky import RunEngine
 from bluesky.callbacks.mpl_plotting import initialize_qt_teleporter
 from bluesky.callbacks.best_effort import BestEffortCallback
