71d3ad866924db6c3c9e0163b9337e328c0db467
K Lauer <klauer@slac.stanford.edu>
2020-09-16 15:11:24 -0700

    Revert "MNT: remove jedi deactivation, since it doesn't seem like an issue now"
    This reverts commit 293b301b74b952eccdf3314cb64138a0b1ac4575.

--- hutch_python/cli.py
+++ hutch_python/cli.py
@@ -120,6 +120,8 @@ def main():
             logger.warning('No DISPLAY environment variable detected. '
                            'Methods that create graphics will not '
                            'function properly.')
+        # Avoid bugs, probably removable at some point
+        ipy_config.InteractiveShellApp.Completer.use_jedi = False
         # Finally start the interactive session
         start_ipython(argv=['--quick'], user_ns=objs, config=ipy_config)
     else:
