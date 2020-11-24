2b5b3bbcb085267edaaebe7127b676e62f1f760c
Zachary Lentz <zlentz@slac.stanford.edu>
2020-11-24 15:45:50 -0800

    MNT: simplify plan discovery to catch daq_step_scan_decorator

--- hutch_python/plan_defaults.py
+++ hutch_python/plan_defaults.py
@@ -1,5 +1,4 @@
 from importlib import import_module
-from inspect import isgeneratorfunction
 from types import SimpleNamespace
 
 
@@ -18,15 +17,14 @@ def collect_plans(modules):
         for name, obj in module.__dict__.items():
             try:
                 # Only include things that are natively from this module
-                if obj.__module__ == module_name:
-                    try:
-                        # Check the __wrapped__ attribute for decorators
-                        if isgeneratorfunction(obj.__wrapped__):
-                            plans[name] = obj
-                    except AttributeError:
-                        # Not a decorator, check obj
-                        if isgeneratorfunction(obj):
-                            plans[name] = obj
+                from_module = obj.__module__ == module_name
+                # Only include callables
+                is_callable = callable(obj)
+                # Skip hidden items
+                is_hidden = len(name) == 0 or name[0] == '_'
+
+                if from_module and is_callable and not is_hidden:
+                    plans[name] = obj
             except AttributeError:
                 # obj did not have __module__, probably a builtin
                 pass
