35ce9e7ab22b99b4cec8a7cdb38b2b1f60347c2e
Zachary Lentz <zlentz@slac.stanford.edu>
2021-06-03 10:00:23 -0700

    ENH: allow environment variable setting of hutch python ophyd defaults

--- hutch_python/ophyd_settings.py
+++ hutch_python/ophyd_settings.py
@@ -2,12 +2,28 @@
 Module to contain ophyd-specific settings that must be applied prior to
 creating any ophyd objects.
 """
+import os
+
 from ophyd.signal import EpicsSignalBase
+
+# Environment variable definitions
+CONN_VAR = 'HUTCH_PYTHON_CONNECTION_TIMEOUT'
+READ_VAR = 'HUTCH_PYTHON_READ_TIMEOUT'
+WRITE_VAR = 'HUTCH_PYTHON_WRITE_TIMEOUT'
+AUTO_VAR = 'HUTCH_PYTHON_AUTO_MONITOR'
 
 
 def setup_ophyd():
     """
     Call EpicsSignalBase.set_defaults with pcds defaults.
+
+    First, checks the following environment variables to set:
+      - HUTCH_PYTHON_CONNECTION_TIMEOUT -> connection_timeout
+      - HUTCH_PYTHON_READ_TIMEOUT -> timeout
+      - HUTCH_PYTHON_WRITE_TIMEOUT -> write_timeout
+      - HUTCH_PYTHON_AUTO_MONITOR -> auto_monitor
+
+    If missing, default to the following behavior:
 
     Sets the following to the current ophyd defaults:
       - connection_timeout
@@ -21,9 +37,9 @@ def setup_ophyd():
       - write timeout (we don't want to wait forever)
     """
     EpicsSignalBase.set_defaults(
-        connection_timeout=1.0,
-        timeout=2.0,
-        write_timeout=5.0,
-        auto_monitor=False,
+        connection_timeout=os.environ.get(CONN_VAR, 1.0),
+        timeout=os.environ.get(READ_VAR, 2.0),
+        write_timeout=os.environ.get(WRITE_VAR, 5.0),
+        auto_monitor=os.environ.get(AUTO_VAR, False),
         )
 
