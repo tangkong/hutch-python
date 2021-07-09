f048eca8c0da6d957dc8da76c318a41757026038
Zachary Lentz <zlentz@slac.stanford.edu>
2021-07-09 11:39:26 -0700

    STY: flake8 line length

--- hutch_python/mpl_config.py
+++ hutch_python/mpl_config.py
@@ -1,7 +1,7 @@
 # If no environment variable set, try qt5 (best for hutch-python)
 import os
 os.environ.setdefault('MPLBACKEND', 'Qt5Agg')
-# Matplotlib will choose an appropriate backend based on the environment variable
-# settings and its ability to communicate with an X server. Our preference, as above,
-# is to use the Qt5 backend when available.
+# Matplotlib will choose an appropriate backend based on the environment
+# variable settings and its ability to communicate with an X server.
+# Our preference, as above, is to use the Qt5 backend when available.
 import matplotlib.pyplot  # noqa: F401 E402
