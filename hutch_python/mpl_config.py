1825a9d9a5fb37f70bfc168a37ba321594cad2b2
Zachary Lentz <ZLLentz@users.noreply.github.com>
2021-07-09 11:05:20 -0700

    DOC: clarify and improve comment
    Co-authored-by: Ken Lauer <klauer@users.noreply.github.com>

--- hutch_python/mpl_config.py
+++ hutch_python/mpl_config.py
@@ -2,5 +2,7 @@
 # If no environment variable set, try qt5 (best for hutch-python)
 import os
 os.environ.setdefault('MPLBACKEND', 'Qt5Agg')
-# Force matplotlib to pick a backend
+# Matplotlib will choose an appropriate backend based on the environment variable
+# settings and its ability to communicate with an X server. Our preference, as above,
+# is to use the Qt5 backend when available.
 import matplotlib.pyplot
