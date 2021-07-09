573e088c7e8a89544238640bc285232836a0e65a
Zachary Lentz <ZLLentz@users.noreply.github.com>
2021-07-09 11:05:43 -0700

    STY: ignore flake8 as needed here
    Co-authored-by: Ken Lauer <klauer@users.noreply.github.com>

--- hutch_python/mpl_config.py
+++ hutch_python/mpl_config.py
@@ -5,4 +5,4 @@ os.environ.setdefault('MPLBACKEND', 'Qt5Agg')
 # Matplotlib will choose an appropriate backend based on the environment variable
 # settings and its ability to communicate with an X server. Our preference, as above,
 # is to use the Qt5 backend when available.
-import matplotlib.pyplot
+import matplotlib.pyplot  # noqa: F401 E402
