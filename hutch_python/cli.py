e9b25b8d8dcffc42e5654b191703693d60a0ea32
Zachary Lentz <zlentz@slac.stanford.edu>
2021-09-23 11:07:27 -0700

    REF: remove now-redundant sim setter

--- hutch_python/cli.py
+++ hutch_python/cli.py
@@ -12,7 +12,6 @@ import IPython
 import matplotlib
 from cookiecutter.main import cookiecutter
 from IPython import start_ipython
-from pcdsdaq.sim import set_sim_mode as set_daq_sim
 from traitlets.config import Config
 
 from .constants import CONDA_BASE, DIR_MODULE
@@ -140,10 +139,6 @@ def main():
         cookiecutter(str(DIR_MODULE / 'cookiecutter'), no_input=True,
                      extra_context=dict(base=base, env=env, hutch=hutch))
         return
-
-    # Now other flags
-    if args.sim:
-        set_daq_sim(True)
 
     # Save whether we are an interactive session or a script session
     opts_cache['script'] = args.script
