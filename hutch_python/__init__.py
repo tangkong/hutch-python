a5cc7b9953e12e2fad2004f15e804eb6f5b80bc1
tangkong <tangkongrobert@gmail.com>
2022-04-28 12:35:11 -0700

    BLD/STY: Update Versioneer, isort __init__.py

--- hutch_python/__init__.py
+++ hutch_python/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
-from ._version import get_versions
+from . import _version
 
-__version__ = get_versions()['version']
-del get_versions
 logger = logging.getLogger(__name__)
+
+__version__ = _version.get_versions()['version']
