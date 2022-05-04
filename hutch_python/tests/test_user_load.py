ee39011fac063932f664dd3adc2026ec4fb68aa4
tangkong <tangkongrobert@gmail.com>
2022-05-03 17:06:13 -0700

    TST: test_load_tab_whitelist

--- hutch_python/tests/test_user_load.py
+++ hutch_python/tests/test_user_load.py
@@ -1,6 +1,9 @@
 import logging
 
-from hutch_python.user_load import get_user_objs
+from pcdsdevices.interface import BaseInterface, set_engineering_mode
+
+from hutch_python.user_load import get_user_objs, update_whitelist
+from hutch_python.utils import HelpfulNamespace
 
 logger = logging.getLogger(__name__)
 
@@ -14,3 +17,24 @@ def test_user_load():
     assert objs['some_int'] == 5
     assert objs['just_this'] == 5.0
     assert 'not_this' not in objs
+
+
+def test_load_tab_whitelist():
+
+    class MyTest(BaseInterface):
+        THIS_SHOULD_NOT_BE_THERE = None
+        tab_whitelist = ['foo']
+        foo = 'f'
+        bar = 'b'
+
+    set_engineering_mode(False)
+    a = MyTest()
+    ns = HelpfulNamespace(a=a)
+
+    # __dir__() as a proxy for tab completion.  We test tab
+    # completion setup elsewhere.
+    assert 'foo' in a.__dir__()
+    assert 'bar' not in a.__dir__()
+
+    update_whitelist(ns, 'a', ['bar'])
+    assert 'bar' in a.__dir__()
