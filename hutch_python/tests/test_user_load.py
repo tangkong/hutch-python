6089a25c2682fcb6f0b2aff5b2c303a82ae1b6b9
tangkong <tangkongrobert@gmail.com>
2022-05-06 10:07:18 -0700

    TST: add tests for nested namespaces, blacklist cfgs

--- hutch_python/tests/test_user_load.py
+++ hutch_python/tests/test_user_load.py
@@ -1,8 +1,10 @@
 import logging
 
+import pytest
 from pcdsdevices.interface import BaseInterface, set_engineering_mode
 
-from hutch_python.user_load import get_user_objs, update_whitelist
+from hutch_python.user_load import (get_user_objs, update_blacklist,
+                                    update_objs, update_whitelist)
 from hutch_python.utils import HelpfulNamespace
 
 logger = logging.getLogger(__name__)
@@ -19,22 +21,61 @@ def test_user_load():
     assert 'not_this' not in objs
 
 
-def test_load_tab_whitelist():
+class MyTest(BaseInterface):
+    THIS_SHOULD_NOT_BE_THERE = None
+    tab_whitelist = ['foo']
+    foo = 'f'
+    bar = 'b'
 
-    class MyTest(BaseInterface):
-        THIS_SHOULD_NOT_BE_THERE = None
-        tab_whitelist = ['foo']
-        foo = 'f'
-        bar = 'b'
 
+@pytest.fixture
+def objs(request):
     set_engineering_mode(False)
     a = MyTest()
-    ns = HelpfulNamespace(a=a)
+    b = MyTest()
+    if request.param == 'flat':
+        ns = HelpfulNamespace(a=a, b=b)
+    elif request.param == 'nested':
+        ns = HelpfulNamespace(a=a, ns=HelpfulNamespace(b=b))
+    return (ns, a, b)
 
-    # __dir__() as a proxy for tab completion.  We test tab
-    # completion setup elsewhere.
-    assert 'foo' in a.__dir__()
-    assert 'bar' not in a.__dir__()
 
-    update_whitelist(ns, 'a', ['bar'])
-    assert 'bar' in a.__dir__()
+@pytest.mark.parametrize(
+    'objs', ['flat', 'nested'], indirect=True
+)
+def test_indirect(objs):
+    ns, dev_a, dev_b = objs
+    cfg = {'a': {'tab_whitelist': ['bar']},
+           'b': {'tab_whitelist': ['bar', 'dne']}}
+
+    assert 'bar' not in dev_a.__dir__()
+    assert 'bar' not in dev_b.__dir__()
+    update_objs(ns, 'a', cfg['a']['tab_whitelist'],
+                update_whitelist)
+    assert 'bar' in dev_a.__dir__()
+    assert 'bar' not in dev_b.__dir__()
+
+    update_objs(ns, 'b', cfg['b']['tab_whitelist'],
+                update_whitelist)
+    assert 'bar' in dev_b.__dir__()
+    assert 'dne' not in dev_b.__dir__()
+
+
+@pytest.mark.parametrize(
+    'objs', ['flat', 'nested'], indirect=True
+)
+def test_load_tab_blacklist(objs):
+    ns, dev_a, dev_b = objs
+    cfg = {'a': {'tab_blacklist': ['bar']},
+           'b': {'tab_blacklist': ['foo', 'dne']}}
+
+    update_objs(ns, 'a', cfg['a']['tab_blacklist'],
+                update_blacklist)
+    assert 'bar' not in dev_a.__dir__()
+    assert 'foo' in dev_b.__dir__()
+
+    assert 'dne' not in dev_b.__dir__()
+    update_objs(ns, 'b', cfg['b']['tab_blacklist'],
+                update_blacklist)
+    assert 'foo' not in dev_b.__dir__()
+    assert 'dne' not in dev_b.__dir__()
