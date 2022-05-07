534893b4fe02e0461e4c0914c2a2009464fc1e8b
tangkong <tangkongrobert@gmail.com>
2022-05-07 10:56:16 -0700

    MNT: add replace_tablist, kind update, rework cfg loading

--- hutch_python/user_load.py
+++ hutch_python/user_load.py
@@ -1,8 +1,9 @@
 import logging
 from pathlib import Path
-from typing import Callable, Union
+from typing import Callable, Tuple, Union
 
 import yaml
+from ophyd import Kind
 from pcdsdevices.interface import BaseInterface
 
 from . import utils
@@ -57,47 +58,11 @@ def get_user_objs(load, *, ask_on_failure=True):
     return objs
 
 
-def configure_objects(
-    obj_config: Union[str, Path],
-    objs: utils.HelpfulNamespace
-) -> utils.HelpfulNamespace:
-    """
-    Configure objects based on user provided settings.
-
-    Parameters
-    -----------
-    obj_config : Pathlike
-        Path to configuration yml file
-
-    objs : HelpfulNamespsace
-        All objects to be loaded into hutch-python
-
-    Returns
-    -------
-    objs : HelpfulNamespace
-        Modified objects
-    """
-    with utils.safe_load('user object configuration'):
-        with open(obj_config, 'r') as f:
-            cfg = yaml.safe_load(f)
-
-        for entry in cfg:
-            # apply each valid configuration action
-            if 'tab_whitelist' in cfg[entry]:
-                update_objs(objs, entry, cfg[entry]['tab_whitelist'],
-                            update_whitelist)
-            if 'tab_blacklist' in cfg[entry]:
-                update_objs(objs, entry, cfg[entry]['tab_blacklist'],
-                            update_blacklist)
-
-        return objs
-
-
 def update_objs(
     obj_ns: utils.HelpfulNamespace,
     entry: str,
-    attrs: list[str],
-    fn: Callable[[BaseInterface, list[str]], None]
+    attrs: dict,
+    fn: Callable[[BaseInterface, Union[dict, list[str]]], None]
 ) -> None:
     """
     Helper function for updating object namespaces
@@ -115,7 +80,7 @@ def update_objs(
     attrs : list[str]
         List of attributes to add to whitelist
 
-    fn : Callable[[BaseInterface, list[str]], None]
+    fn : Callable[[BaseInterface, dict], None]
         A function that applies a change to a singular device
     """
     dev = getattr(obj_ns, entry, None)
@@ -145,7 +110,7 @@ def update_whitelist(dev: BaseInterface, attrs: list[str]) -> None:
         pcdsdevices.interface.BaseInterface
 
     attrs : list[str]
-        List of attributes to add to whitelist
+        list of attributes to add to ``dev`` tablist
     """
     for att in attrs:
         dev._tab.add(att)
@@ -162,10 +127,104 @@ def update_blacklist(dev: BaseInterface, attrs: list[str]) -> None:
         pcdsdevices.interface.BaseInterface
 
     attrs : list[str]
-        List of attributes to add to blacklist
+        list of attributes to remove from ``dev`` tablist
     """
     for att in attrs:
         try:
             dev._tab.remove(att)
         except KeyError:
             logger.debug(f'key: {att} not in tab completion list')
+
+
+def replace_tablist(dev: BaseInterface, attrs: list[str]) -> None:
+    """
+    Replace tab completion list for ``dev`` with ``attrs``
+
+    Parameters
+    ----------
+    dev : BaseInterface
+        A single object implementing the tab completion features in
+        pcdsdevices.interface.BaseInterface
+
+    attrs : list[str]
+        list of attributes to replace ``dev`` tablist with
+    """
+    for old_att in dir(dev):
+        dev._tab.remove(old_att)
+
+    for att in attrs:
+        dev._tab.add(att)
+
+
+def update_kind(dev: BaseInterface, attrs: dict) -> None:
+    """
+    Update the kind of components in ``dev``
+
+    Parameters
+    ----------
+    dev : BaseInterface
+        A single object implementing the tab completion features in
+        pcdsdevices.interface.BaseInterface
+
+    attrs : dict
+        configuration for kind settings on ``dev``
+    """
+    for cpt, kind in attrs.items():
+        k = getattr(Kind, kind, None)
+        if not k:
+            logger.debug(f'{kind} not a valid kind.')
+            continue
+        c = getattr(dev, cpt, None)
+        if not c:
+            logger.debug(f'Device {dev.name} has no component {cpt}')
+            continue
+
+        c.kind = k
+
+
+# order of operations may matter.
+mods = [
+    ('tab_whitelist', update_whitelist),
+    ('tab_blacklist', update_blacklist),
+    ('replace_tablist', replace_tablist),
+    ('kind', update_kind),
+]
+
+
+def configure_objects(
+    obj_config: Union[str, Path],
+    objs: utils.HelpfulNamespace,
+    mod_list: list[Tuple[str, Callable]] = None
+) -> utils.HelpfulNamespace:
+    """
+    Configure objects based on user provided settings.
+
+    Parameters
+    -----------
+    obj_config : Pathlike
+        Path to configuration yml file
+
+    objs : HelpfulNamespsace
+        All objects to be loaded into hutch-python
+
+    mod_list : list[Tuple[str, Callable]]
+        list of modifications to apply, formatted as a tuple of
+        keys and corresponding functions
+
+    Returns
+    -------
+    objs : HelpfulNamespace
+        Modified objects
+    """
+    with utils.safe_load('user object configuration'):
+        with open(obj_config, 'r') as f:
+            cfg = yaml.safe_load(f)
+        if mod_list is None:
+            mod_list = mods
+
+        for entry in cfg:
+            # apply each valid configuration action
+            for k, fn in mod_list:
+                if k in cfg[entry]:
+                    update_objs(objs, entry, cfg[entry][k], fn)
+        return objs
