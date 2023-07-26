ad64fc9f168f4cb4f6d38d82ef862b81efea8188
Zachary Lentz <zlentz@slac.stanford.edu>
2023-07-26 11:31:12 -0700

    DOC: include docs-versions-menu

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "nbsphinx",
     "sphinx.ext.mathjax",
+    "docs_versions_menu",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
