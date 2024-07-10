70252f8bed2ab497e00324add7deda248b7a0ed2
Jane Liu <janeliu@slac.stanford.edu>
2024-07-10 16:20:13 -0700

    Fixed a few items.

--- hutch_python/tests/test_ipython_session_timer.py
+++ hutch_python/tests/test_ipython_session_timer.py
@@ -16,25 +16,27 @@ def fake_ipython():
 @pytest.fixture(scope='function')
 def session_timer(fake_ipython):
     session_timer = IPythonSessionTimer(fake_ipython)
-    session_timer.max_idle_time = 20.0
+    session_timer.max_idle_time = 5.0
     session_timer.user_active = True
     session_timer.curr_time = time.monotonic()
-    session_timer.last_active_time = session_timer.curr_time - 10.0
+    session_timer.last_active_time = session_timer.curr_time - 2.0
     session_timer.idle_time = session_timer.curr_time - session_timer.last_active_time
     return session_timer
 
 
 def test_set_user_active(session_timer):
+    session_timer._set_user_active()
     assert session_timer.user_active
 
 
 def test_set_user_inactive(session_timer):
-    session_timer.user_active = False
+    session_timer._set_user_inactive()
     assert not session_timer.user_active
 
 
 def test_set_idle_time(session_timer):
-    assert session_timer.idle_time == 10.0
+    session_timer._set_idle_time()
+    assert session_timer.idle_time == pytest.approx(2.0, 0.01)
 
 
 # Skipping tests for _start_session() where self.user_active==True because this enters an
