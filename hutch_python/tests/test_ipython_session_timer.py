70d75aa3d3a89c474e637cd858a5917c37d44156
Jane Liu <janeliu@slac.stanford.edu>
2024-07-10 11:10:37 -0700

    Updated unit test for _start_session() function.

--- hutch_python/tests/test_ipython_session_timer.py
+++ hutch_python/tests/test_ipython_session_timer.py
@@ -10,15 +10,17 @@ from hutch_python.ipython_session_timer import IPythonSessionTimer
 @pytest.fixture(scope='function')
 def fake_ipython():
     fake_ipython = FakeIPython()
+    fake_ipython.ask_exit = lambda *args, **kwargs: None
     return fake_ipython
 
 
 @pytest.fixture(scope='function')
 def session_timer(fake_ipython):
     session_timer = IPythonSessionTimer(fake_ipython)
+    session_timer.max_idle_time = 20.0
     session_timer.user_active = True
     session_timer.curr_time = time.monotonic()
-    session_timer.last_active_time = session_timer.curr_time - 100.0
+    session_timer.last_active_time = session_timer.curr_time - 10.0
     session_timer.idle_time = session_timer.curr_time - session_timer.last_active_time
     return session_timer
 
@@ -33,25 +35,17 @@ def test_set_user_inactive(session_timer):
 
 
 def test_set_idle_time(session_timer):
-    assert session_timer.idle_time == 100.0
+    assert session_timer.idle_time == 10.0
 
 
-# Test case 1: the user is inactive and their idle time is less than the maximum allowable idle time.
+# Skipping tests for _start_session() where self.user_active==True because this enters an
+# infinite while loop.
+
 @unittest.mock.patch('time.sleep', lambda seconds: None)
-def test_start_session_case1(session_timer):
+def test_start_session_case1(session_timer, fake_ipython, capsys):
     session_timer.user_active = False
 
-    if (session_timer.idle_time < session_timer.max_idle_time) or session_timer.user_active:
-        assert session_timer.idle_time == 100.0
+    session_timer._start_session()
 
-
-# Test case 2: the user is inactive and their idle time is equivalent to or exceeds the maximum allowable idle time.
-@unittest.mock.patch('time.sleep', lambda seconds: None)
-def test_start_session_case2(session_timer, capsys):
-    session_timer.user_active = False
-    session_timer.idle_time = session_timer.max_idle_time
-
-    if (session_timer.idle_time >= session_timer.max_idle_time) and not session_timer.user_active:
-        print("This hutch-python session has timed out. Please start a new session.")
-        captured = capsys.readouterr()
-        assert "timed out" in captured.out
+    captured = capsys.readouterr()
+    assert "timed out" in captured.out
