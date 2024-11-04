a3269e1bb079471ec4c0a346ab18d893d9020791
Jane Liu <janeliu@slac.stanford.edu>
2024-11-04 13:45:09 -0800

    Removed redundant code to include the test db beamline.

--- hutch_python/tests/test_happi.py
+++ hutch_python/tests/test_happi.py
@@ -17,9 +17,6 @@ def test_happi_objs():
     logger.debug("test_happi_objs")
     db = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                       'happi_db.json')
-    # patch lightpath configs to include test db beamline
-    conftest.beamlines['TST'] = ['X0']
-    conftest.sources.append('X0')
     # Only select active objects
     lc = get_lightpath(db, 'tst')
     objs = get_happi_objs(db, lc, 'tst')
@@ -44,9 +41,6 @@ def test_load_level(load_level: DeviceLoadLevel, num_devices: int):
     logger.debug("test_load_level")
     db = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                       'happi_db.json')
-    # patch lightpath configs to include test db beamline
-    conftest.beamlines['TST'] = ['X0']
-    conftest.sources.append('X0')
     # Only select active objects
     lc = get_lightpath(db, 'tst')
     objs = get_happi_objs(db, lc, 'tst', load_level=load_level)
@@ -71,9 +65,6 @@ def test_happi_objs_without_exclude_devices():
     logger.debug("test_happi_objs")
     db = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                       'happi_db.json')
-    # patch lightpath configs to include test db beamline
-    conftest.beamlines['TST'] = ['X0']
-    conftest.sources.append('X0')
     # Only select active objects
     lc = get_lightpath(db, 'tst')
     # in DeviceLoadLevel.STANDARD you search happi and look for devices with same beamline name
@@ -87,9 +78,6 @@ def test_happi_objs_with_exclude_devices():
     logger.debug("test_happi_objs")
     db = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                       'happi_db.json')
-    # patch lightpath configs to include test db beamline
-    conftest.beamlines['TST'] = ['X0']
-    conftest.sources.append('X0')
     # Only select active objects
     lc = get_lightpath(db, 'tst')
     # get devices not in excluded_devices list
